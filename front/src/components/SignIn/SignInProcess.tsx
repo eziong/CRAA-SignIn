b472605050984d31d60148bc6b59f8bcb85409ad
eziong <bsjwoong@gmail.com>
2022-08-10 09:58:14 +0900

    text field with mui

--- front/src/components/SignIn/SignInProcess.tsx
+++ front/src/components/SignIn/SignInProcess.tsx
@@ -1,3 +1,39 @@
+import { AlternateEmail, LockOutlined } from "@mui/icons-material";
+import { Button, InputAdornment, TextField } from "@mui/material";
+import { useEffect, useRef } from "react";
+
+import { TextFieldWithIcon } from "modules/TextFieldWithIcon";
+import { useState } from "react";
+
 export function SignInProcess() {
-  return <div>sign in process</div>;
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div>
+      <div>
+        <span>Hello Again!</span>
+        <br />
+        <span>something</span>
+      </div>
+      <div>
+        <TextFieldWithIcon
+          inputRef={emailInputRef}
+          label="Email"
+          type="email"
+          icon={<AlternateEmail />}
+        />
+        <TextFieldWithIcon
+          inputRef={passwordInputRef}
+          label="Password"
+          type="password"
+          icon={<LockOutlined />}
+        />
+      </div>
+      <div>
+        <Button />
+        <Button />
+      </div>
+    </div>
+  );
 }
---
+++ front/src/modules/TextFieldWithIcon.tsx
@@ -0,0 +1,37 @@
+import { InputAdornment, TextField } from "@mui/material";
+
+import { useState } from "react";
+
+export function TextFieldWithIcon({
+  inputRef,
+  label,
+  type,
+  icon,
+}: {
+  inputRef: React.Ref<any>;
+  label: string;
+  type: string;
+  icon?: JSX.Element;
+}) {
+  const [focused, setFocused] = useState<boolean>(false);
+
+  return (
+    <TextField
+      inputRef={inputRef}
+      label={label}
+      type={type}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment
+            position="end"
+            style={{ color: focused ? "blue" : "grey" }}
+          >
+            {icon}
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+}
