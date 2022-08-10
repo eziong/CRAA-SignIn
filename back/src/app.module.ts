4bbd45ac4a1ff421159da09f3f89a898034dc584
eziong <bsjwoong@gmail.com>
2022-08-10 15:03:05 +0900

    feat: auth controller

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,10 @@
+import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
